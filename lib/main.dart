c8a22e389e02eaafc16c723f690e60a28a668c9c
kishor-gadhave <kishorgadhave5511@gmail.com>
2024-06-15 11:32:33 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
       ),
-      home: MyHomepage(),
+      home: const MyHomepage(),
     );
   }
 }
